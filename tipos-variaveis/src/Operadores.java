200621b4d2911f59ee405905c20c4b591a824fb8
Jonathan <jonathan.o.franco@hotmail.com>
2023-08-30 09:34:57 -0300

    Incluindo método de operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,8 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+        String nome = "Jonathan ";
+        String sobreNome = "Franco";
+        String nomeCompleto = nome + sobreNome;
+        System.out.println(nomeCompleto);
+    }
+}
